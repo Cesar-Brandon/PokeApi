c2b261dfad853bd5186f057a0fe65027d14f124c
Cesar Brandon <burandon@bura.com>
2021-12-11 18:41:06 -0500

    Carpetas por funcionalidad - teams

--- api.js
+++ api.js
@@ -2,20 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser')
 //ROUTERS
 const authRoutes = require('./routers/auth').router;
-const teamsRoutes = require('./routers/teams').router;
+const teamsRoutes = require('./teams/teams.router').router;
 
 const app = express();
+
+app.use(express.json());
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-
     res.status(200).send('Hello World!');  
 });
 
 app.use('/auth', authRoutes);
 app.use('/teams', teamsRoutes);
-
 
 app.listen(port, () => {
     console.log('El servidor se inició en el puerto 3000');
