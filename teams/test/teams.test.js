3afa7a32ebd3ff9a69e2d9a7f4597f1facc52c50
Cesar Brandon <burandon@bura.com>
2021-12-11 18:43:01 -0500

    refactor en teams

---
+++ teams/test/teams.test.js
@@ -0,0 +1,94 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const usersController = require('../../controllers/users');
+const teamsController = require('../teams.controller');
+const app = require('../../api').app;
+
+before((done) => {
+    usersController.registerUser('bettatech', '1234');
+    usersController.registerUser('mastermind', '4321');
+    done();
+})
+
+afterEach((done) => {
+    teamsController.cleanUpTeam();
+    done();
+})
+
+describe('Suite de pruebas teams', () => {
+    it('should return the team of the given user', (done) => {
+        // Cuando la llamada tiene correctamente la llave
+        let team = [{name: 'Charizard'}, {name: 'Blastoise'}, {name: 'Pikachu'}];
+        chai.request(app)
+            .post('/auth/login')
+            .set('content-type', 'application/json')
+            .send({user: 'mastermind', password: '4321'})
+            .end((err, res) => {
+                let token = res.body.token;
+                //Expect valid login
+                chai.assert.equal(res.statusCode, 200);
+                chai.request(app)
+                    .put('/teams')
+                    .send({
+                        team: team
+                    })
+                    .set('Authorization', `JWT ${token}`)
+                    .end((err, res) => {
+                        chai.request(app)
+                            .get('/teams')
+                            .set('Authorization', `JWT ${token}`)
+                            .end((err, res) => {
+                                // Tiene un equipo con Charizard & Blastoise
+                                // {trainer: "mastermind", team: [pokemon]}
+                                chai.assert.equal(res.statusCode, 200);
+                                chai.assert.equal(res.body.trainer, 'mastermind');
+                                chai.assert.equal(res.body.team.length, team.length);
+                                chai.assert.equal(res.body.team[0].name, team[0].name);
+                                chai.assert.equal(res.body.team[1].name, team[1].name);
+                                done();
+                            });
+                    });
+            });
+    })
+    it('should return the pokedex number', (done) => {
+        let team = [{name: 'Charizard'}, {name: 'Blastoise'}, {name: 'Pikachu'}];
+        chai.request(app)
+            .post('/auth/login')
+            .set('content-type', 'application/json')
+            .send({user: 'mastermind', password: '4321'})
+            .end((err, res) => {
+                let token = res.body.token;
+                //Expect valid login
+                chai.assert.equal(res.statusCode, 200);
+                chai.request(app)
+                    .put('/teams')
+                    .send({team: team})
+                    .set('Authorization', `JWT ${token}`)
+                    .end((err, res) => {
+                        chai.request(app)
+                            .delete('/teams/pokemons/1')
+                            .set('Authorization', `JWT ${token}`)
+                            .end((err, res) => {
+                                chai.request(app)
+                                    .get('/teams')
+                                    .set('Authorization', `JWT ${token}`)
+                                    .end((err, res) => {
+                                        // tiene equipo con Charizard y Blastoise
+                                        // { trainer: 'mastermind', team: [Pokemon]}
+                                        chai.assert.equal(res.statusCode, 200);
+                                        chai.assert.equal(res.body.trainer, 'mastermind');
+                                        chai.assert.equal(res.body.team.length, team.length - 1);
+                                        done();
+                                    });
+                            });
+                    });
+            });
+    });
+});
+
+after((done) => {
+    usersController.cleanUpUsers();
+    done();
+})
