372ece3bd42f461fba702a91124b93453ad086df
Cesar Brandon <burandon@bura.com>
2021-12-12 00:06:22 -0500

    ordenar test-teams

--- teams/test/teams.test.js
+++ teams/test/teams.test.js
@@ -4,22 +4,22 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const usersController = require('../../auth/users.controller');
 const teamsController = require('../teams.controller');
+
 const app = require('../../app').app;
 
-before((done) => {
-    usersController.registerUser('bettatech', '1234');
-    usersController.registerUser('mastermind', '4321');
-    done();
+beforeEach(async () => {
+    await usersController.registerUser('bettatech', '1234');
+    await usersController.registerUser('mastermind', '4321');
 })
 
-afterEach((done) => {
-    teamsController.cleanUpTeam();
-    done();
+afterEach(async () => {
+    await usersController.cleanUpUsers();
+    await teamsController.cleanUpTeam();
 })
 
 describe('Suite de pruebas teams', () => {
     it('should return the team of the given user', (done) => {
-        // Cuando la llamada tiene correctamente la llave
+        // Cuando la llamada no tiene correctamente la llave
         let team = [{name: 'Charizard'}, {name: 'Blastoise'}, {name: 'Pikachu'}];
         chai.request(app)
             .post('/auth/login')
@@ -40,8 +40,8 @@ describe('Suite de pruebas teams', () => {
                             .get('/teams')
                             .set('Authorization', `JWT ${token}`)
                             .end((err, res) => {
-                                // Tiene un equipo con Charizard & Blastoise
-                                // {trainer: "mastermind", team: [pokemon]}
+                                // tiene equipo con Charizard y Blastoise
+                                // { trainer: 'mastermind', team: [Pokemon]}
                                 chai.assert.equal(res.statusCode, 200);
                                 chai.assert.equal(res.body.trainer, 'mastermind');
                                 chai.assert.equal(res.body.team.length, team.length);
@@ -51,7 +51,8 @@ describe('Suite de pruebas teams', () => {
                             });
                     });
             });
-    })
+    });
+
     it('should return the pokedex number', (done) => {
         let team = [{name: 'Charizard'}, {name: 'Blastoise'}, {name: 'Pikachu'}];
         chai.request(app)
@@ -86,9 +87,71 @@ describe('Suite de pruebas teams', () => {
                     });
             });
     });
-});
 
-after((done) => {
-    usersController.cleanUpUsers();
-    done();
-})
+    it('should remove the pokemon at index', (done) => {
+        // Cuando la llamada no tiene correctamente la llave
+        let pokemonName = 'Bulbasaur';
+        chai.request(app)
+            .post('/auth/login')
+            .set('content-type', 'application/json')
+            .send({user: 'mastermind', password: '4321'})
+            .end((err, res) => {
+                let token = res.body.token;
+                //Expect valid login
+                chai.assert.equal(res.statusCode, 200);
+                chai.request(app)
+                    .post('/teams/pokemons')
+                    .send({name: pokemonName})
+                    .set('Authorization', `JWT ${token}`)
+                    .end((err, res) => {
+                        chai.request(app)
+                            .get('/teams')
+                            .set('Authorization', `JWT ${token}`)
+                            .end((err, res) => {
+                                // tiene equipo con Charizard y Blastoise
+                                // { trainer: 'mastermind', team: [Pokemon]}
+                                chai.assert.equal(res.statusCode, 200);
+                                chai.assert.equal(res.body.trainer, 'mastermind');
+                                chai.assert.equal(res.body.team.length, 1);
+                                chai.assert.equal(res.body.team[0].name, pokemonName);
+                                chai.assert.equal(res.body.team[0].pokedexNumber, 1);
+                                done();
+                            });
+                    });
+            });
+    });
+
+    it('should not be able to add pokemon if you already have 6', (done) => {
+        let team = [
+            {name: 'Charizard'}, 
+            {name: 'Blastoise'}, 
+            {name: 'Pikachu'},
+            {name: 'Charizard'}, 
+            {name: 'Blastoise'}, 
+            {name: 'Pikachu'}];
+        chai.request(app)
+            .post('/auth/login')
+            .set('content-type', 'application/json')
+            .send({user: 'mastermind', password: '4321'})
+            .end((err, res) => {
+                let token = res.body.token;
+                //Expect valid login
+                chai.assert.equal(res.statusCode, 200);
+                chai.request(app)
+                    .put('/teams')
+                    .send({team: team})
+                    .set('Authorization', `JWT ${token}`)
+                    .end((err, res) => {
+                        chai.request(app)
+                            .post('/teams/pokemons')
+                            .send({name: 'Vibrava'})
+                            .set('Authorization', `JWT ${token}`)
+                            .end((err, res) => {
+                                chai.assert.equal(res.statusCode, 400);
+                                done();
+                            });
+                    });
+            });
+    });
+
+});
